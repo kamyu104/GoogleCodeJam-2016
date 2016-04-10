3ef65db7f61dc64cbff9dd44169b46d58d2a2bd8
kamyu <kamyu104@users.noreply.github.com>
2016-04-10 18:38:15 +0800

    Create fractiles.py

---
+++ Qualification-Round/fractiles.py
@@ -0,0 +1,31 @@
+# Time:  O(K)
+# Space: O(1)
+
+def fractiles():
+    K, C, S = map(int, raw_input().strip().split())
+
+    # We can only check C positions in the original artwork by 1 student.
+    if C * S < K:
+        return "IMPOSSIBLE"
+
+    indexes = []
+    for i in xrange(1, K + 1, C):
+        # Let p(j) be a position in the artwork of complexity (j+1) s.t
+        # p(j) = (p(j - 1) - 1) * K + d(j) (1 <= d(j) <= K)
+        # We observe that the value of p(j) is L iff p(j-1) is L, and iff,
+        # ..., and iff p(0) is L, and iff the values of p(0) in the
+        # position d(0) ~ d(C-1) are Ls
+        #
+        # So we want to check if the values of p(0)
+        # in the position i ~ i + C-1 are all Ls,
+        # We may choose d(j) to be min(i + j, K),
+        # thus we only need to check if the value p(C) is L
+        # to get the answer.
+        p = 1
+        for j in xrange(C):
+            p = (p - 1) * K + min(i + j, K)
+        indexes.append(p)
+    return " ".join(map(str, indexes))
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, fractiles())
