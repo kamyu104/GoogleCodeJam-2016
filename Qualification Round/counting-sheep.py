d161f5134146f41dbb8ea08e88683249ea8f9ed9
kamyu <kamyu104@users.noreply.github.com>
2016-04-12 08:44:52 +0800

    Update counting-sheep.py

--- Qualification Round/counting-sheep.py
+++ Qualification Round/counting-sheep.py
@@ -3,7 +3,7 @@
 # Google Code Jam 2016 Qualification Round - Problem A. Counting Sheep
 # https://code.google.com/codejam/contest/6254486/dashboard#s=p0
 #
-# Time:  O(PlogP) = O(10 * N * log(10 *N)) = O(NlogN), P is the smallest power of 10 greater than N
+# Time:  O(PlogP) = O(10 * N * log(10 * N)) = O(NlogN), P is the smallest power of 10 greater than N
 # Space: O(logP) = O(logN)
 #
 
@@ -14,10 +14,10 @@ def counting_sheep():
 
     digits = set([])
     cnt = 0
-    while len(digits) != 10:
+    while len(digits) != 10:  # At most O(P) times.
         cnt += N
         tmp = cnt
-        while tmp:
+        while tmp: # O(logP) times.
             digits.add(tmp % 10)
             tmp /= 10
     return cnt
