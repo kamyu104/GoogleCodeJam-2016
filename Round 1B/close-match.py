71716a5cb438caffd04400c23ba832fa187a65f0
Allen Liu <kamyu@AllendeMacBook-Pro.local>
2016-05-01 11:06:34 +0800

    update

--- Round 1B/close-match.py
+++ Round 1B/close-match.py
@@ -3,48 +3,62 @@
 # Google Code Jam 2016 Round 1B - Problem B. Close Match
 # https://code.google.com/codejam/contest/11254486/dashboard#s=p1
 #
-# Time:  O(N * 3^N)
+# Time:  O(N^2)
 # Space: O(N)
 #
 
-def smallest(S):
-    return S.replace('?', '0')
-
-
-def largest(S):
-    return S.replace('?', '9')
-
-
-def close_match(S, J):
-    if not S:
-        return (0, '', '')
-
-    if S[0] == J[0]:
-        if S[0] == '?':
-            return min(close_match('0' + S[1:], '0' + J[1:]),
-                       close_match('0' + S[1:], '1' + J[1:]),
-                       close_match('1' + S[1:], '0' + J[1:]))
+def awins(A, B, hint=-1):
+    X = list(A)
+    Y = list(B)
+    wins = False;
+    for i in xrange(len(X)):
+        if wins:
+            if X[i] == '?':
+                X[i] = '0'         
+            if Y[i] == '?':
+                Y[i] = '9' 
         else:
-            R = close_match(S[1:], J[1:])
-            return (R[0], S[0] + R[1], J[0] + R[2])
-    elif S[0] == '?':
-        x = int(J[0])
-        return min(close_match(str(x + d) + S[1:], J) \
-                   for d in [-1, 0, 1] if 0 <= x + d <= 9)
-    elif J[0] == '?':
-        x = int(S[0])
-        return min(close_match(S, str(x + d) + J[1:]) \
-                   for d in [-1, 0, 1] if 0 <= x + d <= 9)
-    elif S[0] < J[0]:
-        s = largest(S)
-        j = smallest(J)
-        return (int(j) - int(s), s, j)
-    else:
-        s = smallest(S)
-        j = largest(J)
-        return (int(s) - int(j), s, j)
+            if X[i] != '?' and Y[i] != '?':
+                if X[i] < Y[i]:
+                    return (float("inf"), 0, 0)
+                if X[i] > Y[i]:
+                    wins = True
+            else:
+                if i == hint:
+                    if X[i] == '?' and Y[i] == '?':
+                        X[i], Y[i] = '1', '0'
+                    elif X[i] == '?' and Y[i] != '9':
+                        X[i] = str(int(Y[i])+1)
+                    elif Y[i] == '?' and X[i] != '0':
+                        Y[i] = str(int(X[i])-1)
 
+                    if X[i] != '?' and Y[i] != '?':
+                        wins = True
+
+                if X[i] == '?' and Y[i] == '?':
+                    X[i], Y[i] = '0', '0'
+                elif X[i] == '?':
+                    X[i] = Y[i]
+                elif Y[i] == '?':
+                    Y[i] = X[i]
+
+    X = "".join(X)
+    Y = "".join(Y)
+    return (int(X)-int(Y), X, Y)
+
+
+def bwins(A,B, hint=-1):
+    X = awins(B, A, hint)
+    return (X[0], X[2], X[1])
+
+
+def close_match():
+    A, B = raw_input().strip().split()
+    res = min(awins(A, B),bwins(A, B))
+    for i in xrange(len(A)):
+        res = min(res, min(awins(A, B, i), bwins(A, B, i)))
+        
+    return res
 
 for case in xrange(input()):
-    S, J = raw_input().strip().split()
-    print 'Case #{0}: {2} {3}'.format(case+1, *close_match(S, J))
+    print 'Case #{0}: {2} {3}'.format(case+1, *close_match())
