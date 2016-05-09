d4c4d251fbaba9b6c47c669ab961f59e159d87ce
kamyu <kamyu104@users.noreply.github.com>
2016-05-10 00:02:44 +0800

    Update senate-evacuation.py

--- Round 1C/senate-evacuation.py
+++ Round 1C/senate-evacuation.py
@@ -7,32 +7,32 @@
 # Space: O(P)
 #
 
-import heapq
+from heapq import heappush, heappop
 
 def senate_evacuation():
     N = int(input())
     P = map(int, raw_input().strip().split())
     heap, res = [], []
     for i, n in enumerate(P):
-        heapq.heappush(heap, (-n, chr(ord('A') + i)))
+        heappush(heap, (-n, chr(ord('A') + i)))
 
     while heap:
         if len(heap) >= 3:
             # Evacuating a single senator from the largest party.
-            n1, p1 = heapq.heappop(heap)
+            n1, p1 = heappop(heap)
             res.append(str(p1))
             if n1 + 1 != 0:
-                heapq.heappush(heap, (n1 + 1, p1))
+                heappush(heap, (n1 + 1, p1))
         else:
             if len(heap) == 2:
                 # The two parties must have equal numbers of senators.
                 # We can evacuate them in pairs.
-                n1, p1 = heapq.heappop(heap)
-                n2, p2 = heapq.heappop(heap)
+                n1, p1 = heappop(heap)
+                n2, p2 = heappop(heap)
                 res.append(str(p1) + str(p2))
                 if n1 + 1 != 0:
-                    heapq.heappush(heap, (n1 + 1, p1))
-                    heapq.heappush(heap, (n2 + 1, p2))
+                    heappush(heap, (n1 + 1, p1))
+                    heappush(heap, (n2 + 1, p2))
         
     return " ".join(res)
 
