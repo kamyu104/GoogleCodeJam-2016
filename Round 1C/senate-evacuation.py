f836e5780cf0840fbc70abd31c5bfb646f8ed762
Allen Liu <kamyu@AllendeMBP.Home>
2016-05-09 21:51:22 +0800

    update

--- Round 1C/senate-evacuation.py
+++ Round 1C/senate-evacuation.py
@@ -7,56 +7,56 @@
 # Space: O(P)
 #
 
-import heapq
+from heapq import heappush, heappop
 
 def senate_evacuation():
     N = int(input())
     P = map(int, raw_input().strip().split())
     heap, res = [], []
     for i, n in enumerate(P):
-        heapq.heappush(heap, (-n, chr(ord('A') + i)))
+        heappush(heap, (-n, chr(ord('A') + i)))
 
     # Try to get the top 3 parties (n1, n2, n3)
     while heap:
-        n1, p1 = heapq.heappop(heap)
+        n1, p1 = heappop(heap)
         if heap:
-            n2, p2 = heapq.heappop(heap)
+            n2, p2 = heappop(heap)
             if n1 == n2:
                 if heap:
-                    n3, p3 = heapq.heappop(heap)
+                    n3, p3 = heappop(heap)
                     if n2 == n3:
-                        # Evacuate the top 1 party if n1 = n2 = n3
+                        # Eacute the top 1 party if n1 = n2 = n3
                         res.append(str(p1))
                         if n1 + 1 != 0:
-                            heapq.heappush(heap, (n1 + 1, p1))
-                        heapq.heappush(heap, (n2, p2))
-                        heapq.heappush(heap, (n3, p3))
+                            heappush(heap, (n1 + 1, p1))
+                        heappush(heap, (n2, p2))
+                        heappush(heap, (n3, p3))
                     else:
-                        # Evacuate the last two parties if n1 = n2
+                        # Evacute the last two parties if n1 = n2
                         res.append(str(p1) + str(p2))
                         if n1 + 1 != 0:
-                            heapq.heappush(heap, (n1 + 1, p1))
+                            heappush(heap, (n1 + 1, p1))
                         if n2 + 1 != 0:
-                            heapq.heappush(heap, (n2 + 1, p2))
-                        heapq.heappush(heap, (n3, p3))
+                            heappush(heap, (n2 + 1, p2))
+                        heappush(heap, (n3, p3))
                 else:
-                    # Evacuate the top 2 parties if n1 = n2 > n3
+                    # Evacute the top 2 parties if n1 = n2 > n3
                     res.append(str(p1) + str(p2))
                     if n1 + 1 != 0:
-                        heapq.heappush(heap, (n1 + 1, p1))
+                        heappush(heap, (n1 + 1, p1))
                     if n2 + 1 != 0:
-                        heapq.heappush(heap, (n2 + 1, p2))
+                        heappush(heap, (n2 + 1, p2))
             else:
-                # Evacuate the top 1 party if n1 > n2
+                # Evacute the top 1 party if n1 > n2
                 res.append(str(p1))
                 if n1 + 1 != 0:
-                    heapq.heappush(heap, (n1 + 1, p1))
-                heapq.heappush(heap, (n2, p2))
+                    heappush(heap, (n1 + 1, p1))
+                heappush(heap, (n2, p2))
         else:
-            # Evacuate the last one party.
+            # Evacute the last one party.
             res.append(str(p1))
             if n1 + 1 != 0:
-                heapq.heappush(heap, (n1 + 1, p1))
+                heappush(heap, (n1 + 1, p1))
         
     return " ".join(res)
 
