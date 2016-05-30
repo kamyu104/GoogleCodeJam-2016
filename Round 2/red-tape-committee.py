5c5fd60120e8b4e9b34d127f5d2c0b82ff5b56e6
kamyu <kamyu104@users.noreply.github.com>
2016-05-30 05:32:58 -0500

    Update red-tape-committee.py

--- Round 2/red-tape-committee.py
+++ Round 2/red-tape-committee.py
@@ -17,7 +17,7 @@ def red_tape_committee():
         # from one or both extremes.
         V = P[:M] + P[N-K+M:]
 
-        # dp[i][j]: i members with j yes votes.
+        # dp[i][j]: the probability of i members with j yes votes.
         dp = [[1.0], []]
         for i in xrange(1, K + 1):
             dp[i % 2] = [0] * (len(dp[(i - 1) % 2]) + 1)
@@ -25,7 +25,8 @@ def red_tape_committee():
                 dp[i % 2][j] += dp[(i - 1) % 2][j] * (1 - V[i - 1])  # vote no
                 dp[i % 2][j + 1] += dp[(i - 1) % 2][j] * V[i - 1]  # vote yes
 
-        # tie is K members with (K/2) yes votes.
+        # The probability of tie is the probability of
+        # K members with (K/2) yes votes.
         result = max(result, dp[K % 2][K / 2])
 
     return result
