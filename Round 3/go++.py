f1b26b754af7fe5af2957d547890e869d8aec5e8
Allen Liu <kamyu@AllendeMBP.Home>
2016-06-13 23:24:28 +0800

    update

--- Round 3/go++.py
+++ Round 3/go++.py
@@ -23,7 +23,7 @@ def gopp():
 
     P1, P2 = [], []
     for c in B:
-        # Because executing each pair of ("10", "1?") and ("01", "0?")
+        # Because executing each matchable pair of ("10", "1?") and ("01", "0?")
         # could result in either 1 or 0, the written P1 and P2 produce a set of
         # all possible strings consisting of 0s and/or 1s.
         if c == '0':
@@ -32,7 +32,7 @@ def gopp():
         else:
             P1 += "01",
             P2 += "0?",
-    # Remove the last "01"/"10" of P1 to reduce the count of 0s and 1s by one.
+    # Remove the last "01"/"10" of P1 to reduce the count of matchable 0s and 1s by one.
     # It could still produce a set of all possible strings consisting of 0s and/or 1s
     # except for B because the count of matchable 0s/1s is not enough to make.
     P1.pop()
