a1fe25c3a9aca5f9cd1f8eb7d8b7723e38d2f70b
kamyu <kamyu104@users.noreply.github.com>
2016-08-08 11:52:28 +0800

    Update family-hotel.py

--- World Finals/family-hotel.py
+++ World Finals/family-hotel.py
@@ -29,7 +29,7 @@ s = [0] * 3
 f[0] = 1
 s[0], s[1] = f[0], f[0] + f[1]
 for n in xrange(2, MAX_N):
-    # f[n]: the probability of first two rooms are occupied in the n rooms.
+    # f[n]: the probability of the first two rooms are assigned in the n rooms.
     # f[0] = 1, f[1] = 0
     # f[n] = 1/n * (1 + f[1] + f[2] + ... + f[n-2])
     f[n] = mul(inv(n), s[(n-2) % 3])
