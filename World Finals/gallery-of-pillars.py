465e2f090e927eb6dcea481a4db84ba86454d7ef
Allen Liu <kamyu104@gmail.com>
2019-09-22 02:28:23 +0800

    update

--- World Finals/gallery-of-pillars.py
+++ World Finals/gallery-of-pillars.py
@@ -4,15 +4,17 @@
 # https://code.google.com/codejam/contest/7234486/dashboard#s=p2
 #
 # Time:  O(NlogN)
-# Space: O(N)
+# Space: O(M)
 #
 
 from math import sqrt
 
-def count(side_len, r_square):
+def count(side_len, r_square):  # Time: O(side_length) = O(N/k), Space: O(1)
     # for 0 <= x, y <= side_len, (x, y) != (0, 0), count (x, y) s.t. x^2 + y^2 <= r^2
     result = 0
-    y = min(side_len, int(sqrt(r_square)))
+    y = side_len
+    if r_square < side_len**2:
+        y = int(sqrt(r_square))
     for x in xrange(y+1):
         while x*x + y*y > r_square:
             y -= 1
@@ -25,23 +27,23 @@ def gallery_of_pillars():
     # count pairs of |(x, y)| < M/R and gcd(x, y) = 1
     result = 0
     r_square = (M*M-1)//R//R
-    for k in xrange(1, int(sqrt(r_square))+1):
+    for k in xrange(1, int(sqrt(r_square))+1):  # O(sum of (N/k)) = O(NlogN)
         if MOBIOUS[k] and (N-1)//k >= 1:
             result += MOBIOUS[k] * count((N-1)//k, r_square//k//k)
     return result
 
-def sieve_of_eratosthenes(N):
-    is_prime = [True]*N
-    mobious = [1]*N
-    for i in xrange(2, N):
+def sieve_of_eratosthenes(n):  # Time: O(M), Space: O(M)
+    is_prime = [True]*n
+    mobious = [1]*n
+    for i in xrange(2, n):
         if not is_prime[i]:
             continue
-        for j in xrange(i+i, N, i):
+        for j in xrange(i+i, n, i):
             is_prime[j] = False
-        for j in xrange(i, N, i):
+        for j in xrange(i, n, i):
             mobious[j] = -mobious[j]
-        if i <= N//i:
-            for j in xrange(i*i, N, i*i):
+        if i <= n//i:
+            for j in xrange(i*i, n, i*i):
                 mobious[j] = 0
     return mobious
 
