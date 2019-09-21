9ad20e8825a881cb8df43545ed8997c92d12ff5e
kamyu <kamyu104@users.noreply.github.com>
2019-09-22 03:17:02 +0800

    Update gallery-of-pillars.py

--- World Finals/gallery-of-pillars.py
+++ World Finals/gallery-of-pillars.py
@@ -24,7 +24,7 @@ def count(side_len, r_square):  # Time: O(side_length) = O(N/k), Space: O(1)
 
 def gallery_of_pillars():
     N, R = map(int, raw_input().strip().split())
-    # count pairs of |(x, y)| < M/R and gcd(x, y) = 1
+    # count pairs of |(x, y)| < M/R and 0 <= x, y <= N-1 and gcd(x, y) = 1
     result = 0
     r_square = (M*M-1)//R//R
     for k in xrange(1, min(N-1, int(sqrt(r_square)))+1):  # sum of O(N/k) = O(NlogN)
