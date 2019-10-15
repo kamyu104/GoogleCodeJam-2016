06becb790cbd0fa8b138e8a2a8a4955162900173
Allen Liu <kamyu104@gmail.com>
2019-10-15 23:44:20 +0800

    update

--- World Finals/radioactive-islands.py
+++ World Finals/radioactive-islands.py
@@ -22,30 +22,33 @@ def D(C, x):
     return dose
 
 def calc(C, a, b):  # dose = (1 + sum(1 / (x^2 + (y-ci)^2))) * sqrt(dx^2 + dy^2)
-    return (1.0+D(C, (X_START + a[0]*X_STEP, Y_START + a[1]*Y_STEP))) * \
+    return (1.0+D(C, (X_START + a[0]*X_STEP, MIN_Y_BOUND + a[1]*Y_STEP))) * \
            sqrt(((b[0]-a[0])*X_STEP)**2 + ((b[1]-a[1])*Y_STEP)**2)
 
 def radioactive_islands():
     N, A, B = map(float, raw_input().strip().split())
     C = map(float, raw_input().strip().split())
     dp = [float("inf")]*(Y_GRID_NUM+1)
-    dp[int((A-Y_START)/Y_STEP + 0.5)] = 0.0
+    dp[int((A-MIN_Y_BOUND)/Y_STEP + 0.5)] = 0.0
     for i in xrange(1, X_GRID_NUM+1):
         new_dp = [float("inf")]*(Y_GRID_NUM+1)
         for j in xrange(Y_GRID_NUM+1):
             for k in xrange(max(0, j-NEIGHBORS_NUM), min(Y_GRID_NUM, j+NEIGHBORS_NUM)+1):
                 new_dp[j] = min(new_dp[j], dp[k] + calc(C, (i-1, k), (i, j)))
         dp = new_dp
-    return dp[int((B-Y_START)/Y_STEP + 0.5)]
+    return dp[int((B-MIN_Y_BOUND)/Y_STEP + 0.5)]
 
 GRANULARITY = 26  # tuned by experiment
 GRID_NUM = 40  # tuned by experiment
-MAX_ABS_SLOPE = 2  # verified by experiment
-Y_START, Y_END = -13.0, 13.0  # verified by experiment
 
+MIN_Y_BOUND, MAX_Y_BOUND = -13.0, 13.0  # verified by experiment
 X_START, X_END = -10.0, 10.0
+MIN_A, MAX_A = -10.0, 10.0
+MIN_C, MAX_C = -10.0, 10.0
+X_C = 0.0
+MAX_ABS_SLOPE = int((MAX_C-MIN_A)/(X_C-X_START))
 X_GRID_NUM, Y_GRID_NUM = GRID_NUM, GRANULARITY*GRID_NUM
-X_STEP, Y_STEP = (X_END-X_START)/X_GRID_NUM, (Y_END-Y_START)/Y_GRID_NUM
+X_STEP, Y_STEP = (X_END-X_START)/X_GRID_NUM, (MAX_Y_BOUND-MIN_Y_BOUND)/Y_GRID_NUM
 NEIGHBORS_NUM = MAX_ABS_SLOPE*Y_GRID_NUM//X_GRID_NUM
 for case in xrange(input()):
     print "Case #%d: %s" % (case+1, radioactive_islands())
